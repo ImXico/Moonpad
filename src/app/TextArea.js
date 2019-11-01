feb0545c1356b649acd35b65dca278a166cc5a64
Xico <fjm.cunha@campus.fct.unl.pt>
2019-11-01 19:20:36 +0000

    File and folder reordering

--- src/app/TextArea.js
+++ src/app/TextArea.js
@@ -3,13 +3,6 @@ import './App.css';
 
 class TextArea extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'Welcome to scribbl!'
-    }
-  }
-
   render() {
     return (
       <div contenteditable="true" style={{minHeight:'50px', width:'300px'}} className="TextArea cursor">
