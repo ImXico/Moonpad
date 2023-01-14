bbb9b7b384117d3003c61f4233f0eb9b85b4439d
Francisco Cunha <francisco.cunha789@gmail.com>
2023-01-14 16:00:06 +0000

    Format src/containers/*

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,20 +1,21 @@
-import { connect } from 'react-redux';
-import { showToastPopup } from '../actions/toastPopup';
-import { toggleColorThemeAndPersist } from '../actions/colorTheme';
-import App from '../components/App/App';
+import { connect } from "react-redux";
+import { showToastPopup } from "../actions/toastPopup";
+import { toggleColorThemeAndPersist } from "../actions/colorTheme";
+import App from "../components/App/App";
 
 const mapStateToProps = (state, ownProps) => {
   return {
     isDarkTheme: state.isDarkTheme,
-    hasCustomTitleBar: ownProps.hasCustomTitleBar
-  }
-}
+    hasCustomTitleBar: ownProps.hasCustomTitleBar,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    showToast: message => dispatch(showToastPopup(message)),
-    toggleColorTheme: isNowLightMode => dispatch(toggleColorThemeAndPersist(isNowLightMode))
-  }
-}
+    showToast: (message) => dispatch(showToastPopup(message)),
+    toggleColorTheme: (isNowLightMode) =>
+      dispatch(toggleColorThemeAndPersist(isNowLightMode)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/containers/TextAreaContainer.js
+++ src/containers/TextAreaContainer.js
@@ -1,21 +1,24 @@
-import { connect } from 'react-redux'
-import { updateTabContentAndPersist } from '../actions/tabs';
-import TextArea from '../components/TextArea/TextArea';
+import { connect } from "react-redux";
+import { updateTabContentAndPersist } from "../actions/tabs";
+import TextArea from "../components/TextArea/TextArea";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const { tabs, selectedTab } = state;
+
   return {
     currentlyActiveTab: selectedTab,
-    currentTabContent: selectedTab !== null
-      ? tabs.find(tab => tab.id === selectedTab).content
-      : ''
-  }
-}
+    currentTabContent:
+      selectedTab !== null
+        ? tabs.find((tab) => tab.id === selectedTab).content
+        : "",
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    updateTabContent: (id, newContent) => dispatch(updateTabContentAndPersist(id, newContent))
-  }
-}
+    updateTabContent: (id, newContent) =>
+      dispatch(updateTabContentAndPersist(id, newContent)),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(TextArea);
