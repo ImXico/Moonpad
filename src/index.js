a52bfeb3b64e06096e1c4e41e69ea65da60d169a
Francisco Cunha <francisco.cunha789@gmail.com>
2023-01-14 15:50:15 +0000

    Format src/index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-import { Provider } from 'react-redux';
-import { createLogger } from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import * as ipcActions from './data/ipcActions';
-import * as serviceWorker from './serviceWorker';
-import AppContainer from './containers/AppContainer';
-import './reset.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import * as ipcActions from "./data/ipcActions";
+import * as serviceWorker from "./serviceWorker";
+import AppContainer from "./containers/AppContainer";
+import "./reset.scss";
 
-const { ipcRenderer } = window.require('electron');
+const { ipcRenderer } = window.require("electron");
 
 /**
  * Set the initial state based on what we have on our local database.
  * This request needs to be done synchronously, and the ipcMain will
  * only listen to it once (and unregister after that).
  */
-const persistedInitialState = ipcRenderer.sendSync(ipcActions.LOAD_PERSISTED_DATA);
+const persistedInitialState = ipcRenderer.sendSync(
+  ipcActions.LOAD_PERSISTED_DATA
+);
 
 /**
  * Check if the app is running in macOS.
@@ -34,9 +36,9 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppContainer hasCustomTitleBar={!isMacOS}/>
+    <AppContainer hasCustomTitleBar={!isMacOS} />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
