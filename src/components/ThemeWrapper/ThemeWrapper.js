108c38bfc47550b02169c78b4e194d909334d133
Francisco Cunha <francisco.cunha789@gmail.com>
2023-01-15 21:39:26 +0000

    Fix linter errors on Themes.js

--- src/components/ThemeWrapper/ThemeWrapper.js
+++ src/components/ThemeWrapper/ThemeWrapper.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import { Dark, Light } from './Themes';
+import React from "react";
+import { Dark, Light } from "./Themes";
 
 // NOTE:
 // Got this from https://medium.com/trabe/a-simple-react-theme-component-using-css-variables-e20434ae97c
 
 class ThemeWrapper extends React.Component {
-
   node = React.createRef();
 
   componentDidMount() {
@@ -19,23 +18,19 @@ class ThemeWrapper extends React.Component {
   }
 
   updateCSSVariables() {
-    Object
-      .entries(this.props.theme)
-      .forEach(([prop, value]) => this.node.current.style.setProperty(prop, value));
+    Object.entries(this.props.theme).forEach(([prop, value]) =>
+      this.node.current.style.setProperty(prop, value)
+    );
   }
 
   render() {
-    return (
-      <div ref={this.node}>
-        {this.props.children}
-      </div>
-    );
+    return <div ref={this.node}>{this.props.children}</div>;
   }
 }
 
 export const Themes = {
   Dark,
-  Light
-}
+  Light,
+};
 
 export default ThemeWrapper;
