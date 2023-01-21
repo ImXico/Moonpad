af5d0613f3b108e54b7eb3dbfb18ca7cfdc1669c
Francisco Cunha <francisco.cunha789@gmail.com>
2023-01-21 21:45:48 +0000

    Add type to ThemeWrapper

---
+++ src/components/ThemeWrapper/ThemeWrapper.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+
+// NOTE:
+// Got this from https://medium.com/trabe/a-simple-react-theme-component-using-css-variables-e20434ae97c
+
+export const enum Themes {
+  Dark,
+  Light,
+}
+
+type Props = {
+  theme: Themes;
+  children: JSX.Element;
+};
+
+export function ThemeWrapper({ theme, children }: Props) {
+  const nodeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    Object.entries(theme).forEach(([prop, value]) => {
+      if (nodeRef.current) {
+        nodeRef.current.style.setProperty(prop, value);
+      }
+    });
+  }, [theme]);
+
+  return <div ref={nodeRef}>{children}</div>;
+}
