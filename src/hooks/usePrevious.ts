54cab27c7a5b8af51208714b77723583a2afc722
Francisco Cunha <francisco.cunha789@gmail.com>
2023-01-22 19:47:24 +0000

    Add types to usePrevious hook

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 
-export function usePrevious<T>(value: T): T {
-  const ref: any = useRef<T>();
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>();
   useEffect(() => {
     ref.current = value;
   });
