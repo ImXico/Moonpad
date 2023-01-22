ca016b282e826dd47dc49812ba758dca0de81c27
Francisco Cunha <francisco.cunha789@gmail.com>
2023-01-22 19:48:00 +0000

    Replace module.exports in defaultSettings

---
+++ src/data/defaultSettings.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_IS_DARK_THEME = true;
+export const DEFAULT_IS_TAB_AREA_OPEN = true;
+export const DEFAULT_IS_WINDOW_ALWAYS_ON_TOP = false;
+export const DEFAULT_WINDOW_WIDTH = 770;
+export const DEFAULT_WINDOW_HEIGHT = 450;
+export const DEFAULT_MIN_WINDOW_WIDTH = 580;
+export const DEFAULT_MIN_WINDOW_HEIGHT = 355;
