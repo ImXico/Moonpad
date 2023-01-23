5cdfc641188113a32235cb780d1b7ac9eca04a75
Francisco Cunha <francisco.cunha789@gmail.com>
2023-01-23 21:12:19 +0000

    Fix ThemeWrapper (needs to be refactored)

--- src/components/ThemeWrapper/ThemeWrapper.tsx
+++ src/components/ThemeWrapper/ThemeWrapper.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef } from "react";
+import { Light as LightTheme, Dark as DarkTheme } from "./Themes";
 
 // NOTE:
 // Got this from https://medium.com/trabe/a-simple-react-theme-component-using-css-variables-e20434ae97c
 
 export const enum Themes {
-  Dark,
-  Light,
+  Dark = "Dark",
+  Light = "Light",
 }
 
 type Props = {
@@ -16,8 +17,11 @@ type Props = {
 export function ThemeWrapper({ theme, children }: Props) {
   const nodeRef = useRef<HTMLDivElement>(null);
 
+  const themeObject: { [colorKey: string]: string } =
+    theme === Themes.Dark ? DarkTheme : LightTheme;
+
   useEffect(() => {
-    Object.entries(theme).forEach(([prop, value]) => {
+    Object.entries(themeObject).forEach(([prop, value]) => {
       if (nodeRef.current) {
         nodeRef.current.style.setProperty(prop, value);
       }
